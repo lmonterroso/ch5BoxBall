2abb27e7c35c7786b54e1d9de1ed76e19ac2614b
Luis Monterroso <43154683+lmonterroso@users.noreply.github.com>
2018-10-15 14:05:50 -0400

    Completed Main onjectives

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -9,6 +9,9 @@ import java.awt.Color;
  *
  * @author Michael Kölling and David J. Barnes
  * @version 2011.07.31
+ * 
+ * @author Luis Monterroso
+ * @version 2018-October-14
  */
 
 public class BallDemo   
@@ -52,5 +55,46 @@ public class BallDemo
                 finished = true;
             }
         }
+    }
+    
+    /**
+     * creates a box and fills it with balls heading in random directions;
+     * @param balls the number of balls you want to spawn in the box
+     * @param height sets the height of the box in pixels
+     * @param length sets the length of the box in pixels 
+     */
+    public void boxBounce(int balls, int height, int length)
+    {
+        //Random color is used to create random colors for the balls
+        Random color = new Random();
+        
+        //ball1 is used to draw the box, originally I had the walls drawn straight from this method but I implemented it into BoxBall 
+        BoxBall ball1 = new BoxBall(16, Color.BLUE, myCanvas, length, height);
+        
+        //The array is used to organize all the balls and make it easy to perform operations on every ball
+        BoxBall[] Balls = new BoxBall[balls];
+        ball1.drawBox();
+        
+        // creates a BoxBall for every element in the array, and draws it when it's instatiated. 
+        for (int i = 0; i < Balls.length; i++)
+        {
+            //generates a random color for the ball, limited to 215 to make the balls more visible.
+            Color newColor = new Color(color.nextInt(215),color.nextInt(215),color.nextInt(215));
+            
+            Balls[i] = new BoxBall(16, newColor, myCanvas, length, height);
+            Balls[i].draw();
+        }
+        boolean finished =  false; 
+        while(!finished) {
+            myCanvas.wait(25);           // small delay
+            for (BoxBall ball: Balls) 
+            {
+                ball.move(); 
+            }
+            if(ball1.getXPosition() >= 550) //finished is always true so the simulation won't end
+                finished = true;
+        }
+        
+        
     }
 }
---
+++ BoxBall.java
@@ -0,0 +1,128 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.Random;
+
+/**
+ * Write a description of class BoxBall here.
+ *
+ * @author Luis Monterroso
+ * @version 10/15/18 
+ */
+public class BoxBall
+{
+    private Ellipse2D.Double circle;
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;     
+    private Canvas canvas;   
+    private Random rand = new Random();
+    
+    /*
+     * The yvelocity is calculated with a random number between 1 and 6,
+     * while the xvelocity is calculated by subtracting yvelocity from 7,
+     * this ensures all the balls move at the same speed but in random directions.
+     */
+    private int yvelocity = rand.nextInt(7) + 1;
+    private int xvelocity = 7 - yvelocity;
+    private int ground;
+    private int ceiling;
+    private int leftWall;
+    private int rightWall;
+
+    /**
+     * Constructor for objects of class BoxBall
+     * @param ballDiameter  the diameter of the ball in pixels;
+     * @param ballColor  the color of the ball
+     * @param drawingCanvas  the canvas to draw this ball on
+     * @param length the length of your box in pixels
+     * @param height the height of your box in pixels
+     */
+    public BoxBall(int ballDiameter, Color ballColor,
+                    Canvas drawingCanvas, int length, int height)
+    {
+        //the four walls are calculated by taking the center of the canvas and adding or subtracting half the length or height
+        ground = 250 + height/2;
+        ceiling = 250 - height/2;
+        leftWall = 300 - length/2;
+        rightWall = 300 + length/2;
+        
+        diameter = ballDiameter;
+        
+        // x and y positions are calculated randomly by taking the values between the walls minus
+        // the diameter to prevent them from spawning on the walls of the box.
+        xPosition = rand.nextInt((rightWall - leftWall) - diameter) + leftWall;
+        yPosition = rand.nextInt(ground - ceiling - diameter) + ceiling;
+        color = ballColor;
+        canvas = drawingCanvas;
+    }
+    
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+    
+    /**
+     * Draws the walls of the box.
+     */
+    public void drawBox ()
+    {
+        canvas.drawLine(leftWall,ground,rightWall,ground);
+        canvas.drawLine(leftWall, ceiling, rightWall, ceiling);
+        canvas.drawLine(leftWall,ground,leftWall,ceiling);
+        canvas.drawLine(rightWall,ground,rightWall,ceiling);
+    }
+    
+    /**
+     * draws the circle at its x and y position;
+     */
+    public void draw()
+    {
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+    
+      /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }   
+    
+    /**
+     * moves the circle and redraws the circle at its new position, if the circle hits
+     * a wall it will rebound in the opposite direction.
+     */
+    public void move()
+    {
+        //velocity controls how fast the circle moves
+        erase();
+        xPosition += xvelocity;
+        yPosition += yvelocity;
+        
+        //if the ball hits a wall flip either the x or y velocity
+        if(yPosition >= (ground - diameter))  {
+            yPosition = (int)(ground - diameter);
+            yvelocity = yvelocity * -1; 
+                
+        } 
+        if(yPosition <= (ceiling + 1))  {
+            yPosition = (int)(ceiling + 1);
+            yvelocity = yvelocity * -1; 
+                
+        }   
+        if (xPosition >= (rightWall - diameter)) {
+            xPosition = (int)(rightWall - diameter);
+            xvelocity *= -1;
+        }
+        if (xPosition <= (leftWall + 1)) {
+            xPosition = (int)(leftWall + 1);
+            xvelocity *= -1;
+        }
+        draw();
+    }
+}
